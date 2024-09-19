7cb585b32a23ece1b0695f850d2c4171577b245d
Pedro Antonio S. <111252527+PA-PedroAntonio@users.noreply.github.com>
2024-09-19 08:35:54 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -31,6 +31,7 @@ function mostraAlternativas() {
     for (const alternativa of perguntaAtual.alternativa) {
         const botaoAlternativas = document.createElement("button");
         botaoAlternativas.textContent = alternativa;
+        caixaAlternativa.appendChild(botaoAlternativas);
     }
 }
 
